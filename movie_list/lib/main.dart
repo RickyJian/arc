7f1b45132f18f968d43fc241acdf72fc7527b3ca
RickyJian <rjricky03bc@gmail.com>
2021-04-04 23:42:01 +0800

    add swiper

--- movie_list/lib/main.dart
+++ movie_list/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:movie_list/bloc/movie.dart';
+import 'package:movie_list/model/item.dart' as model;
 import 'package:sizer/sizer.dart';
 
 void main() => runApp(MyApp());
@@ -27,16 +29,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> images = [
-    "assets/inception.jpg",
-    "assets/jurassic_world.jpg",
-    "assets/knives_out.jpg",
-    "assets/little_women.jpg",
-    "assets/mission_impossible.jpg",
-  ];
-
   @override
   Widget build(BuildContext context) {
+    bloc.fetchAllMovies();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -46,88 +41,98 @@ class _MyHomePageState extends State<MyHomePage> {
           return OrientationBuilder(
             builder: (context, orientation) {
               SizerUtil().init(constraints, orientation);
-              return Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100.0.w,
-                      height: 60.0.h,
-                      padding: EdgeInsets.all(1.0.h),
-                      child: Swiper(
-                        itemBuilder: (BuildContext context, int index) {
-                          return Image.asset(images[index]);
-                        },
-                        itemCount: images.length,
-                        viewportFraction: 0.7,
-                        scale: 0.9,
-                      ),
-                    ),
-                    Container(
-                      width: 100.0.w,
-                      height: 40.0.h,
-                      padding: EdgeInsets.all(3.0.h),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                'Name:',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                              Text(
-                                'Inception',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                'Release Date:',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                              Text(
-                                '2020-10-01',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                'Rate:',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                              Text(
-                                '85',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: <Widget>[
-                              Text(
-                                'Description:',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                              Text(
-                                'rich text',
-                                style: TextStyle(fontSize: 30),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+              return StreamBuilder(
+                stream: bloc.allMovies,
+                builder: (context, AsyncSnapshot<model.Item> snapshot) {
+                  return buildSwiper(snapshot);
+                },
               );
             },
           );
         },
+      ),
+    );
+  }
+
+  Widget buildSwiper(AsyncSnapshot<model.Item> snapshot) {
+    var a = '';
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 100.0.w,
+            height: 60.0.h,
+            padding: EdgeInsets.all(1.0.h),
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return Image.asset(snapshot.data.movies[index].path);
+              },
+              itemCount: 5,
+              viewportFraction: 0.7,
+              scale: 0.9,
+            ),
+          ),
+          Container(
+            width: 100.0.w,
+            height: 40.0.h,
+            padding: EdgeInsets.all(3.0.h),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Name:',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      'Inception',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Release Date:',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      '2020-10-01',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Rate:',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      '85',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      'Description:',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      'rich text',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

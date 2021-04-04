19efb8968a4791938e7562cd2407288242f3d65e
RickyJian <rjricky03bc@gmail.com>
2021-04-04 23:40:01 +0800

    merge item and movie model

--- movie_list/lib/model/item.dart
+++ movie_list/lib/model/item.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
+import 'dart:core';
 
 import 'package:flutter/services.dart' show rootBundle;
-
-import 'movie.dart';
 
 class Item {
   List _movieAssets = [
@@ -13,15 +12,52 @@ class Item {
     "assets/mission_impossible.json",
   ];
 
-  Future<List<Movie>> fetchMovies() async {
-    List<Movie> movieList = [];
-    for (int i = 0; i < _movieAssets.length; i++) {
+  int _total;
+  List<_Movie> _movieList = [];
+
+  Future<Item> fetchMovies() async {
+    var item = Item();
+    var movieLen = _movieAssets.length;
+    List<_Movie> movieList = [];
+    for (int i = 0; i < movieLen; i++) {
       await getMovie(_movieAssets[i]).then((value) => movieList.add(value));
     }
-    return movieList;
+    item._total = movieLen;
+    item._movieList = movieList;
+    return item;
   }
 
-  Future<Movie> getMovie(String path) => rootBundle
+  Future<_Movie> getMovie(String path) => rootBundle
       .loadString(path)
-      .then((value) => Movie.fromJson(jsonDecode(value)));
+      .then((value) => _Movie.fromJson(jsonDecode(value)));
+
+  int get total => _total;
+
+  List<_Movie> get movies => _movieList;
+}
+
+class _Movie {
+  String _name;
+  DateTime _releaseDate;
+  double _rate;
+  String _description;
+  String _path;
+
+  _Movie.fromJson(Map<String, dynamic> json) {
+    _name = json["name"];
+    _releaseDate = DateTime.parse(json["release_date"]);
+    _rate = json["rate"] as double;
+    _description = json["description"];
+    _path = json["path"];
+  }
+
+  String get name => _name;
+
+  DateTime get releaseDate => _releaseDate;
+
+  double get rate => _rate;
+
+  String get description => _description;
+
+  String get path => _path;
 }

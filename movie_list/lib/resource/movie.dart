d1c35acfac0ed1d7e378ee8ffc391c952ee34e9a
RickyJian <rjricky03bc@gmail.com>
2021-04-05 19:27:00 +0800

    add provider

--- movie_list/lib/resource/movie.dart
+++ movie_list/lib/resource/movie.dart
@@ -1,7 +1,5 @@
 import 'package:movie_list/model/item.dart';
 
 class MovieProvider {
-  Future<Item> fetchAllMovies() async {
-    return await Item().fetchMovies();
-  }
+  Future<Item> fetchAllMovies() async => await Item().fetchMovies();
 }

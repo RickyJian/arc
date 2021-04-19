6b2fd47c7a8dd3dd1c0cc95a04051981e9a659e8
RickyJian <rjricky03bc@gmail.com>
2021-04-19 23:18:08 +0800

    add start and tap event

--- bottom_navigation/lib/bloc/bottom_navigation_event.dart
+++ bottom_navigation/lib/bloc/bottom_navigation_event.dart
@@ -2,3 +2,19 @@ part of 'bottom_navigation_bloc.dart';
 
 @immutable
 abstract class BottomNavigationEvent {}
+
+// AppStarted defines event when application started
+class AppStarted extends BottomNavigationEvent {
+  @override
+  String toString() => 'AppStarted';
+}
+
+// PageTapped defines event when tapped bottom navigation item
+class PageTapped extends BottomNavigationEvent {
+  final int index;
+
+  PageTapped({@required this.index}) : super();
+
+  @override
+  String toString() => 'PageTapped: $index';
+}

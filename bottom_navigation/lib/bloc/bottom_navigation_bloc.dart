084fc351f1eba80cdaaa4081e787067dbeefcb7a
RickyJian <rjricky03bc@gmail.com>
2021-04-21 21:42:56 +0800

    add navigation bloc

--- bottom_navigation/lib/bloc/bottom_navigation_bloc.dart
+++ bottom_navigation/lib/bloc/bottom_navigation_bloc.dart
@@ -4,15 +4,33 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'bottom_navigation_event.dart';
+
 part 'bottom_navigation_state.dart';
 
-class BottomNavigationBloc extends Bloc<BottomNavigationEvent, BottomNavigationState> {
+class BottomNavigationBloc
+    extends Bloc<BottomNavigationEvent, BottomNavigationState> {
+  int currentIndex = 0;
+
   BottomNavigationBloc() : super(BottomNavigationInitial());
 
   @override
   Stream<BottomNavigationState> mapEventToState(
     BottomNavigationEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is AppStarted) {
+      this.add(PageTapped(index: currentIndex));
+    } else if (event is PageTapped) {
+      this.currentIndex = event.index;
+      yield CurrentIndexChanged(currentIndex: this.currentIndex);
+      yield PageLoading();
+
+      if (this.currentIndex == 0) {
+        yield FirstPageLoaded(text: "first");
+      } else if (this.currentIndex == 1) {
+        yield SecondPageLoaded(text: "second");
+      } else if (this.currentIndex == 2) {
+        yield ThirdPageLoaded(text: "third");
+      }
+    }
   }
 }

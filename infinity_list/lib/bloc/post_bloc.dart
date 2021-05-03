868bad628f78d5834a510df7f2c2af7a9cffd576
RickyJian <rjricky03bc@gmail.com>
2021-05-03 23:05:26 +0800

    add bloc

---
+++ infinity_list/lib/bloc/post_bloc.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:infinity_list/model/model.dart';
+import 'package:rxdart/rxdart.dart';
+
+part 'post_event.dart';
+
+part 'post_state.dart';
+
+const int _postLimit = 20;
+const String _url = 'https://jsonplaceholder.typicode.com';
+const String _method = '/posts';
+
+class PostBloc extends Bloc<PostEvent, PostState> {
+  final http.Client httpClient;
+
+  PostBloc({@required this.httpClient}) : super(PostState());
+
+  @override
+  Stream<Transition<PostEvent, PostState>> transformEvents(
+      Stream<PostEvent> events,
+      TransitionFunction<PostEvent, PostState> transitionFn) {
+    return super.transformEvents(
+      events.debounceTime(const Duration(milliseconds: 500)),
+      transitionFn,
+    );
+  }
+
+  @override
+  Stream<PostState> mapEventToState(
+    PostEvent event,
+  ) async* {
+    if (event is PostFetch) {
+      yield await _mapPostFetchedToState(state);
+    }
+  }
+
+  Future<PostState> _mapPostFetchedToState(PostState state) async {
+    if (state.hasReachedMax) {
+      return state;
+    }
+
+    try {
+      if (state.status == PostStatus.initial) {
+        final posts = await _fetchPosts();
+        return state.copyWith(
+          status: PostStatus.success,
+          posts: posts,
+          hasReachedMax: false,
+        );
+      }
+
+      final posts = await _fetchPosts(state.posts.length);
+      return posts.isEmpty
+          ? state.copyWith(hasReachedMax: true)
+          : state.copyWith(
+              status: PostStatus.success,
+              posts: List.of(state.posts)..addAll(posts),
+              hasReachedMax: false,
+            );
+    } on Exception {
+      return state.copyWith(status: PostStatus.failed);
+    }
+  }
+
+  Future<List<Post>> _fetchPosts([int startIndex = 0]) async {
+    final response = await httpClient.get(Uri.https(_url, _method,
+        <String, String>{'_start': '$startIndex', '_limit': '$_postLimit'}));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+      return data.map((dynamic json) {
+        return Post(
+          id: json['id'] as int,
+          title: json['title'] as String,
+          body: json['body'] as String,
+        );
+      }).toList();
+    }
+    throw Exception('error fetching posts');
+  }
+}

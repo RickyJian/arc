db5523ce99c58cebe1e02183158131e00854fb75
RickyJian <rjricky03bc@gmail.com>
2021-12-07 23:37:06 +0800

    add persistence navigator project

---
+++ persistence_navigator/android/app/src/main/kotlin/com/example/persistence_navigator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.persistence_navigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ persistence_navigator/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Persistence Navigator',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Persistence Navigator'),
+      ),
+      body: const Center(
+        child: Text('Home page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'Mail',
+          ),
+        ],
+      ),
+    );
+  }
+}

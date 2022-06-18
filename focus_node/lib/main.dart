1de6fc949359a93d0d224b76f2f44408f2fe6f57
RickyJian <rjricky03bc@gmail.com>
2022-06-18 20:04:45 +0800

    init focus node

---
+++ focus_node/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Focus Node Example',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Focus Node Example'),
+      ),
+      body: const Center(
+        child: Text('Init Project'),
+      ),
+    );
+  }
+}
